497d595078094b3551f05c0b61928cd81ecaadd4
Christiane <christianevillaroel@gmail.com>
2024-10-28 10:51:23 -0400

    DashboardView
    Still finalizing dashboard

--- Invertory-App/Invertory-App/Color.swift
+++ Invertory-App/Invertory-App/Color.swift
@@ -9,5 +9,6 @@ import SwiftUI
 extension Color
 {
     public static let myColor: Color = Color(UIColor(red: 0.68, green: 0.41, blue: 0.00, alpha: 1.00))
+    public static let lightGray: Color = Color(UIColor(red: 0.93, green: 0.92, blue: 0.93, alpha: 1.00))
     
 }
