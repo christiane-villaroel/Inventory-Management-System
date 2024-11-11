e70019b9de641204a361e63419c45fe13bbd4605
Brandon <133907617+Thunderbro27@users.noreply.github.com>
2024-11-11 11:59:31 -0500

    Restore

---
+++ Invertory-App/Invertory-App/SideMenuHeaderView.swift
@@ -0,0 +1,39 @@
+//
+//  SideMenuHeaderView.swift
+//  Invertory-App
+//
+//  Created by Brandon Mccoll on 10/30/24.
+//
+
+import SwiftUI
+
+
+
+struct SideMenuHeaderView: View {
+    
+    var body: some View {
+        HStack{
+            Image(systemName: "person.circle.fill")
+                .imageScale(.large)
+                .foregroundStyle(.white)
+                .frame(width: 48, height: 48)
+                .background(Color.myColor)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.vertical)
+          
+            VStack(alignment: .leading, spacing: 6){
+                Text("Place Holder")
+                    .font(.subheadline)
+                Text("placeholder123@gmail.com")
+                    .font(.footnote)
+                    .tint(.gray)
+                
+            }//vstack end
+        }//hstack end
+    
+    }//body end
+}// view end
+
+#Preview {
+    SideMenuHeaderView()
+}
